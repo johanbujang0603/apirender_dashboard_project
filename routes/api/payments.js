bdba70082dbd9083d80f525b4892c59c1804a38f
Zhao <russeldevsmart1998@gmail.com>
2021-07-24 02:32:02 +0300

    fix: charge api

--- routes/api/payments.js
+++ routes/api/payments.js
@@ -31,6 +31,7 @@ router.post("/charge", async (req, res) => {
     const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
     stripe.customers.list({email: userEmail})
     .then(async(customers) => {
+      console.log(customers);
       if (customers.data.length > 0) {
         return customers.data[0].id;
         // makePayment(stripe, totalAmount, body.token.id, projectID, customerID, res);
@@ -49,6 +50,7 @@ router.post("/charge", async (req, res) => {
       }
     })
     .then((customer) => {
+      console.log("customer:", customer);
       try {
         stripe.charges.create({
           amount: parseInt(totalAmount * 100),
